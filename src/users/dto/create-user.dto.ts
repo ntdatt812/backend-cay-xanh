99f438399c98768691db8f9ef815922790bf3269
ntdatt812 <ntdat812@gmail.com>
2025-02-11 22:01:09 +0700

    22.5 validate

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,15 @@
+import { IsEmail, IsEmpty, IsNotEmpty } from "class-validator";
+
 //data tranfer object
 export class CreateUserDto {
+    @IsEmail({}, { message: 'Email không đúng định dạng' })
+    @IsNotEmpty({ message: 'Email không được để trống' })
     email: string;
+
+    @IsNotEmpty({ message: 'Password không được để trống' })
     password: string;
+
     name: string;
+
     address: string;
 }
