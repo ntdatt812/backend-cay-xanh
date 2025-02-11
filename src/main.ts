78249b211779c0f18f0e3371a7e0da9f976f775c
ntdatt812 <ntdat812@gmail.com>
2025-02-11 22:30:26 +0700

    25 update user

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,9 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
