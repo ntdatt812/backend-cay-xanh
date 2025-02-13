aa5571df30f89db22d7d258198dda1d95820ffaf
ntdatt812 <ntdat812@gmail.com>
2025-02-13 09:39:34 +0700

    39

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,19 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private configService: ConfigService
-
+    private configService: ConfigService,
+    private authService: AuthService
   ) { }
-
-  @Get()
-  @Render("home")
-  handleHomePage() {
-    console.log("check port = ", this.configService.get<string>("PORT"))
-    const message1 = this.appService.getHello();
-    return {
-      message: message1
-    }
-    // return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post("/login")
+  handleLogin(@Request() req) {
+    return this.authService.login(req.user)
   }
 }
