ad8e443fabc0702822153fc5e8e3d777009cfaff
ntdatt812 <ntdat812@gmail.com>
2025-02-18 11:27:42 +0700

    refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,16 @@ import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { IUser } from 'src/users/users.interface';
 import { RegisterUserDto } from 'src/users/dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
+import ms from 'ms';
 
 @Injectable()
 export class AuthService {
 
     constructor(
         private usersService: UsersService,
-        private jwtService: JwtService
+        private jwtService: JwtService,
+        private configService: ConfigService,
     ) { }
 
     //username và pass là 2 tham số thư viện passport ném về
@@ -34,19 +37,35 @@ export class AuthService {
             email,
             role
         };
+
+        const refresh_token = this.createRefreshToken(payload)
+
         return {
             access_token: this.jwtService.sign(payload),
-            _id,
-            name,
-            email,
-            role
+            refresh_token,
+            user: {
+                _id,
+                name,
+                email,
+                role
+            }
+
         };
     }
+
     async register(user: RegisterUserDto) {
         const newUser = await this.usersService.register(user)
         return {
             _id: newUser?._id,
             createdAt: newUser?.createdAt
         }
+    }
+
+    createRefreshToken = (payload: any) => {
+        const refresh_token = this.jwtService.sign(payload, {
+            secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET"),
+            expiresIn: ms(this.configService.get<string>("JWT_REFRESH_EXPIRE")) / 1000
+        });
+        return refresh_token;
     }
 }
