0fd12eb5745bcbece20493154320784a53cc99c5
ntdatt812 <ntdat812@gmail.com>
2025-02-18 13:56:26 +0700

    69

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Public, ResponseMessage } from 'src/decorator/customize';
+import { Public, ResponseMessage, User } from 'src/decorator/customize';
 import { LocalAuthGuard } from './local-auth.guard';
 import { RegisterUserDto } from 'src/users/dto/create-user.dto';
 import { Request, Response } from 'express';
+import { IUser } from 'src/users/users.interface';
 
 
 @Controller("auth")
@@ -29,4 +30,19 @@ export class AuthController {
         return this.authService.register(registerUserDto)
     }
 
+    @ResponseMessage("Get user information")
+    @Get("/account")
+    handleGetAccount(@User() user: IUser) {
+        return { user }
+    }
+
+    @Public()
+    @ResponseMessage("Get user by refresh token")
+    @Get("/refresh")
+    handleRefreshToken(
+        @Req() request: Request,
+        @Res({ passthrough: true }) response: Response) {
+        const refreshToken = request.cookies["refresh_token"]
+        return this.authService.processNewToken(refreshToken, response)
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { IUser } from 'src/users/users.interface';
 import { RegisterUserDto } from 'src/users/dto/create-user.dto';
 import { ConfigService } from '@nestjs/config';
 import ms from 'ms';
-import { Response } from 'express';
+import e, { Response } from 'express';
 
 @Injectable()
 export class AuthService {
@@ -28,8 +28,8 @@ export class AuthService {
         return null;
     }
 
-    async login(users: IUser, response: Response) {
-        const { _id, name, email, role } = users;
+    async login(user: IUser, response: Response) {
+        const { _id, name, email, role } = user;
         const payload = {
             sub: "token login",
             iss: "from server",
@@ -76,5 +76,56 @@ export class AuthService {
             expiresIn: ms(this.configService.get<string>("JWT_REFRESH_EXPIRE")) / 1000
         });
         return refresh_token;
+    }
+
+    processNewToken = async (refreshToken: string, response) => {
+        try {
+            this.jwtService.verify(refreshToken, {
+                secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET"),
+            })
+
+            const user = await this.usersService.findUserByToken(refreshToken);
+            if (user) {
+                //update refresh token
+                const { _id, name, email, role } = user;
+                const payload = {
+                    sub: "token refresh",
+                    iss: "from server",
+                    _id,
+                    name,
+                    email,
+                    role
+                };
+
+                const refresh_token = this.createRefreshToken(payload)
+
+                //update user with refresh token
+                await this.usersService.updateUserToken(refresh_token, _id.toString());
+
+                //set refresh token as cookies
+                response.clearCookie("refresh_token"); // xoá cái cũ
+                response.clearCookie("key"); // xoá cái cũ
+
+                response.cookie('refresh_token', refresh_token, {
+                    httpOnly: true,
+                    maxAge: ms(this.configService.get<string>("JWT_REFRESH_EXPIRE"))
+                })
+
+                return {
+                    access_token: this.jwtService.sign(payload),
+                    user: {
+                        _id,
+                        name,
+                        email,
+                        role
+                    }
+
+                };
+            } else {
+                throw new BadRequestException("Refresh token không hợp lệ, vui lòng login!")
+            }
+        } catch (error) {
+            throw new BadRequestException("Refresh token không hợp lệ, vui lòng login!")
+        }
     }
 }
